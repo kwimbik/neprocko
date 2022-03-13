7ec7bcb716505805a3bb51623316b730e5f73d46
kwimbik <kwimbiks@gmail.com>
2022-03-13 15:38:46 +0100

    8/99

--- 99-prolog-problems.pl
+++ 99-prolog-problems.pl
@@ -23,3 +23,54 @@ last_but_one([_|X], L) :- last_but_one(X, L).
 
 element_at(X,[X|T],K) :- length([X|T], K).
 element_at(X,[_|Y],C) :- element_at(X,Y,C).
+
+%vzorove:
+%element_at(X,[X|_],1).
+%element_at(X,[_|L],K) :- K > 1, K1 is K - 1, element_at(X,L,K1).
+
+%1.04 (*) Find the number of elements of a list.
+elems_in_list(0, []).
+elems_in_list(C,[_|Xs]) :-  elems_in_list(C1,Xs), C is C1 + 1.
+
+
+%P05 (*) Reverse a list.
+list_reverse([],[]).
+list_reverse([X|Xs],Y) :- list_reverse(Xs,Z), append(Z,[X],Y).
+
+
+%1.06 (*) Find out whether a list is a palindrome.
+palindrom(L) :- list_reverse(L,L).
+
+%1.07 (**) Flatten a nested list structure.
+%Transform a list, possibly holding lists as elements into a 'flat' list by replacing each list with its elements (recursively).
+
+%Example:
+%?- my_flatten([a, [b, [c, d], e]], X).
+%X = [a, b, c, d, e]
+
+my_flatten(X,[X]) :- \+ is_list(X).
+my_flatten([],[]).
+my_flatten([X|Xs],Y) :- my_flatten(X,Z), my_flatten(Xs,R), append(Z, R, Y).
+
+%1.08a (**) Eliminate duplicates of list elements.
+%Example:
+%?- compress([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+%X = [a,b,c,a,d,e]
+
+remove_duplicate([],[]).
+remove_duplicate([X|Xs], [X|Y]) :-  \+ member(X,Xs), !, remove_duplicate(Xs, Y).
+remove_duplicate([_|Xs], Y) :-  remove_duplicate(Xs, Y).
+
+
+
+%1.08b (**) Eliminate consecutive duplicates of list elements.
+%If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+
+%Example:
+%?- compress([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+%X = [a,b,c,a,d,e]
+
+compress([],[]).
+compress([X|[]],[X]).
+compress([X,X|Xs], Y) :-  compress([X|Xs], Y).
+compress([X,Z|Xs], [X|Y]) :- X \= Z, compress([Z|Xs], Y).
