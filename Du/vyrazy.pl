a5d1fac6ffd75f0136a437694b13721ab9f6849e
kwimbik <kwimbiks@gmail.com>
2022-03-28 15:47:37 +0200

    oprava_du

--- Du/vyrazy.pl
+++ Du/vyrazy.pl
@@ -1,21 +1,11 @@
 %vlozOperatory(+L, -H, -V) kde L je list cisel, H je hodnota vysledneho vyrazu a V je vyraz s kombinaci operatoru
 vlozOperatory([], [], []).
-vlozOperatory(L, H, V) :- createExpression(L, V), eval(V, H).
+vlozOperatory(L, H, E) :-  cet(L, V), writeexp(V, E), eval(V, H). 
 
+cet([X], X).
+cet([X1, X2], Y) :- (Y = plus(X1, X2); Y = minus(X1, X2); Y = krat(X1, X2); Y = deleno(X1, X2)).
+cet(X, Y) :- length(X,K3), K3 > 2, (Y = plus(R1, R2), append(X1, X2, X), length(X1,K1), length(X2, K2), K1 >0, K2 > 0, cet(X1, R1), cet(X2, R2); Y = minus(R1, R2),append(X1, X2, X),length(X1,K1), length(X2, K2), K1 >0, K2 > 0, cet(X1, R1), cet(X2, R2); Y = krat(R1, R2), append(X1, X2, X),length(X1,K1), length(X2, K2), K1 >0, K2 > 0, cet(X1, R1), cet(X2, R2); Y = deleno(R1, R2), append(X1, X2, X),length(X1,K1), length(X2, K2), K1 >0, K2 > 0, cet(X1, R1), cet(X2, R2)).
 
-
-%createExpression
-createExpression([X1, X2], Y) :- (Y = plus(X1, X2); Y = minus(X1, X2); Y = krat(X1, X2); Y = deleno(X1, X2)).
-createExpression([X|Xs], Y) :-
-(createExpression(Xs, X1), Y = plus(X, X1);
-createExpression(Xs, X1), Y = minus(X, X1);
-createExpression(Xs, X1), Y = krat(X, X1);
-createExpression(Xs, X1), Y = deleno(X, X1)).
-
-
-%chooseTwo(+List, -A, -B, -Rest)
-chooseTwo([], [], [], []).
-chooseTwo(L, A, B, R) :- member(A, L), member(B, L), A \= B, delete([A,B], L, R).
 
 %eval(+Expr, -Value)
 eval([], []).
@@ -37,9 +27,19 @@ integer(X1),\+ integer(X2), eval(X2, Y2), Y is X1 * Y2;
 \+ integer(X1),integer(X2), eval(X1, Y1), Y is X2 * Y1;
 eval(X1, Y1), eval(X2, Y2), Y is Y1 * Y2).
 
+eval(X, Y) :- X = deleno(X1, X2), 
+(integer(X1),integer(X2), del(X1,X2, Y);
+integer(X1),\+ integer(X2), eval(X2, Y2), del(X1,Y2, Y);
+\+ integer(X1),integer(X2), eval(X1, Y1), del(Y1,X2, Y);
+eval(X1, Y1), eval(X2, Y2), del(Y1,Y2, Y)).
 
-eval(X, Y) :- X = deleno(X1, X2),
-(integer(X1),integer(X2), Y is X1 / X2;
-integer(X1),\+ integer(X2), eval(X2, Y2), Y is X1 / Y2;
-\+ integer(X1),integer(X2), eval(X1, Y1), Y is X2 / Y1;
-eval(X1, Y1), eval(X2, Y2), Y is Y1 / Y2).
+del(X1,X2, Y) :- not(X2 is 0), Y is X1 // X2.
+del(_,_, 0).
+
+
+%create expression from postfix
+writeexp(X, X) :- integer(X).
+writeexp(X, Y) :- X = plus(X1, X2), Y = (Y1 + Y2), writeexp(X1, Y1), writeexp(X2, Y2).
+writeexp(X, Y) :- X = minus(X1, X2), Y = (Y1 - Y2), writeexp(X1, Y1), writeexp(X2, Y2).
+writeexp(X, Y) :- X = krat(X1, X2), Y = (Y1 * Y2), writeexp(X1, Y1), writeexp(X2, Y2).
+writeexp(X, Y) :- X = deleno(X1, X2), Y = (Y1 / Y2), writeexp(X1, Y1), writeexp(X2, Y2).
