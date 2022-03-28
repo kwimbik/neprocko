2ed22a42af637c96d2ca2815a7ff93b0f3aba2d8
kwimbik <kwimbiks@gmail.com>
2022-03-28 16:30:45 +0200

    vyrazy_done

--- Du/vyrazy.pl
+++ Du/vyrazy.pl
@@ -1,6 +1,6 @@
 %vlozOperatory(+L, -H, -V) kde L je list cisel, H je hodnota vysledneho vyrazu a V je vyraz s kombinaci operatoru
 vlozOperatory([], [], []).
-vlozOperatory(L, H, E) :-  cet(L, V), writeexp(V, E), eval(V, H). 
+vlozOperatory(L, H, E) :-  cet(L, V), writeexp(V, E), ev(V, H). 
 
 cet([X], X).
 cet([X1, X2], Y) :- (Y = plus(X1, X2); Y = minus(X1, X2); Y = krat(X1, X2); Y = deleno(X1, X2)).
@@ -9,16 +9,11 @@ cet(X, Y) :- length(X,K3), K3 > 2, (Y = plus(R1, R2), append(X1, X2, X), length(
 
 %eval(+Expr, -Value)
 eval([], []).
-eval(X, Y) :- X = plus(X1, X2),
-(integer(X1),integer(X2), Y is X1 + X2;
-integer(X1),\+ integer(X2), eval(X2, Y2), Y is X1 + Y2;
-\+ integer(X1),integer(X2), eval(X1, Y1), Y is X2 + Y1;
-eval(X1, Y1), eval(X2, Y2), Y is Y1 + Y2).
-
+eval(X, Y) :- X = plus(X1, X2),(integer(X1),integer(X2), Y is X1 + X2;integer(X1),\+ integer(X2), eval(X2, Y2), Y is X1 + Y2;\+ integer(X1),integer(X2), eval(X1, Y1), Y is Y1 + X2;eval(X1, Y1), eval(X2, Y2), Y is Y1 + Y2).
 eval(X, Y) :- X = minus(X1, X2),
 (integer(X1),integer(X2), Y is X1 - X2;
 integer(X1),\+ integer(X2), eval(X2, Y2), Y is X1 - Y2;
-\+ integer(X1),integer(X2), eval(X1, Y1), Y is X2 - Y1;
+\+ integer(X1),integer(X2), eval(X1, Y1), Y is Y1 - X2;
 eval(X1, Y1), eval(X2, Y2), Y is Y1 - Y2).
 
 eval(X, Y) :- X = krat(X1, X2),
@@ -35,6 +30,14 @@ eval(X1, Y1), eval(X2, Y2), del(Y1,Y2, Y)).
 
 del(X1,X2, Y) :- not(X2 is 0), Y is X1 // X2.
 del(_,_, 0).
+
+
+ev(X, X) :- integer(X).
+ev(X, Y) :- X = plus(X1, X2), ev(X1, R1), ev(X2, R2), Y is R1 + R2.
+ev(X, Y) :- X = minus(X1, X2),  ev(X1, R1), ev(X2, R2), Y is R1 - R2.
+ev(X, Y) :- X = krat(X1, X2), ev(X1, R1), ev(X2, R2), Y is R1 * R2.
+ev(X, Y) :- X = deleno(X1, X2), ev(X1, R1), ev(X2, R2), del(R1, R2, Y).
+
 
 
 %create expression from postfix
